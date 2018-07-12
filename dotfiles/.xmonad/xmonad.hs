e1a2a45218e1fbe33d3ba31079f5addd3a5c113e
Brandon <brandon.owen@hotmail.com>
2018-07-13 09:50:28 +1000

    additionalKeysP

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -3,7 +3,7 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.NoBorders
-import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Run (spawnPipe)
 import Graphics.X11.ExtraTypes.XF86
 import System.IO (hPutStrLn)
@@ -11,7 +11,7 @@ import System.IO (hPutStrLn)
 main :: IO ()
 main = do
     xmproc <- spawnPipe "xmobar"
-    spawn "feh --bg-tile ~/.background.png"
+    spawn "~/.fehbg"
     xmonad $ def
         { terminal = "xterm -sl 1000"
         , manageHook = manageDocks <+> manageHook def
@@ -26,17 +26,17 @@ main = do
         , focusedBorderColor = "#1d1f21"
         , normalBorderColor = "#000000"
         , focusFollowsMouse = False
-        } `additionalKeys`
+        } `additionalKeysP`
         [
-        ((mod1Mask, xK_p), spawn "dmenu_run -fn inconsolata-18")
-        , ((mod1Mask, xK_apostrophe), sendMessage ToggleStruts)
-        , ((0, xF86XK_AudioMute), spawn "amixer -q set Master toggle")
-        , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q set Master 5%+")
-        , ((0, xF86XK_AudioLowerVolume), spawn "amixer -q set Master 5%-")
-        , ((0, xF86XK_AudioMicMute), spawn "amixer -q set Capture toggle")
-        , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10")
-        , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
-        , ((0, xK_Print), spawn "scrot /tmp/selection.png && xclip -selection clipboard -t image/png -i /tmp/selection.png")
-        , ((mod1Mask, xK_Print), spawn "scrot -s /tmp/selection.png && xclip -selection clipboard -t image/png -i /tmp/selection.png")
-        , ((mod1Mask, xK_l), spawn "sudo slock")
+        ("M-p", spawn "dmenu_run -fn inconsolata-18")
+        , ("M-'", sendMessage ToggleStruts)
+        , ("<xF86AudioMute>", spawn "amixer -q set Master toggle")
+        , ("<xF86AudioRaiseVolume>", spawn "amixer -q set Master 5%+")
+        , ("<xF86AudioLowerVolume>", spawn "amixer -q set Master 5%-")
+        , ("<xF86AudioMicMute>", spawn "amixer -q set Capture toggle")
+        , ("<xF86MonBrightnessUp>", spawn "xbacklight -inc 10")
+        , ("<xF86MonBrightnessDown>", spawn "xbacklight -dec 10")
+        , ("<xPrint>", spawn "scrot /tmp/selection.png && xclip -selection clipboard -t image/png -i /tmp/selection.png")
+        , ("M-<xPrint>", spawn "scrot -s /tmp/selection.png && xclip -selection clipboard -t image/png -i /tmp/selection.png")
+        , ("M-l", spawn "sudo slock")
         ]
